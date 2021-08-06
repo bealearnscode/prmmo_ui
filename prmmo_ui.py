5125ceed41dffd594065138c4e288b76bd17edae
B <bealearnscode@gmail.com>
2021-08-06 17:04:33 -0400

    add title and input select dropdown

---
+++ prmmo_ui.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.sidebar.title('PRIMO Search')
+# st.sidebar.text_input('Enter keyword or NIIN')
+
+# col1, col2 = st.columns(2)
+# with col1:
+#     st.header('Lifecycle')
+
+# with col2:
+    # st.header('Dates')
+
+#-- Set search by NIIN or keyword
+select_event = st.sidebar.selectbox('How do you want to find data?',
+                                    ['By NIIN', 'By Keyword'])
+
+if select_event == 'By NIIN':      
+    str_t0 = st.sidebar.text_input('NIIN', '13754920')
+    t0 = int(str_t0)
+
+    st.sidebar.markdown("""
+    Example NIINs:
+    * 6843419
+    * 12822053
+    * 13926982
+    """)
+else:
+    str_t0 = st.sidebar.text_input('Keyword', 'O-RING')
+    t0 = str(str_t0)
+
+    st.sidebar.markdown("""
+    Example Keywords:
+    * O-RING
+    * CONNECTOR
+    * ELECTRON TUBE
+    """)
+
+
